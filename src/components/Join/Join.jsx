35c5765712bf145cd769b9ce57854e5c52f3a276
Deepak kumar <87894208+Deepak6o@users.noreply.github.com>
2023-04-27 21:29:16 +0530

    Update Join.jsx

--- src/components/Join/Join.jsx
+++ src/components/Join/Join.jsx
@@ -1,27 +1,56 @@
-import React from "react";
-import './Join.css';
+import React, { useRef } from "react";
+import "./Join.css";
+import emailjs from "@emailjs/browser";
 
-const Join = () =>{
-    return (
-        <div className="Join" id="join-us">
-            <div className="left-j">
-            <hr/>
-                <div>
-                    <span className="stroke-text">READY TO </span>
-                    <span>LEVEL UP</span>
-                </div>
-                <div>
-                    <span>YOUR BODY</span>
-                    <span className="stroke-text"> WITH US ?</span>
-                </div>
-            </div>
-            <div className="right-j">
-                <form action="" className="email-container">
-                    <input type="email" name="user_email" placeholder="Enter Your Email Address here"/>
-                    <button className="btn btn-j">Join Now</button>
-                </form>
-            </div>
+const Join = () => {
+  const form = useRef();
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        'service_04pd4xb',
+        'template_rvkkj0p',
+        form.current,
+        '6w0mxIsPu41gawOww'
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+  return (
+    <div className="Join" id="join-us">
+      <div className="left-j">
+        <hr />
+        <div>
+          <span className="stroke-text">READY TO </span>
+          <span>LEVEL UP</span>
         </div>
-    )
-}
+        <div>
+          <span>YOUR BODY</span>
+          <span className="stroke-text"> WITH US ?</span>
+        </div>
+      </div>
+      <div className="right-j">
+        <form
+          ref={form}
+          className="email-container"
+          onSubmit={sendEmail}
+        >
+          <input
+            type="email"
+            name="user_email"
+            placeholder="Enter Your Email Address here"
+          />
+          <button className="btn btn-j">Join Now</button>
+        </form>
+      </div>
+    </div>
+  );
+};
 export default Join;
